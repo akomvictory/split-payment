a0b8012f0d7918392fe1f16e2f72efcac003263c
akom <akom.victory@gmail.com>
2024-02-05 11:23:54 +0100

    first commit

---
+++ paystack.php
@@ -0,0 +1,106 @@
+//after users registeration 
+
+<?php
+  $url = "https://api.paystack.co/subaccount";
+
+  $fields = [
+    'business_name' => "user name",
+    'settlement_bank' => "bank name", 
+    'account_number' => "0193274682", 
+    'percentage_charge' => 10 
+  ];
+
+  $fields_string = http_build_query($fields);
+
+  //open connection
+  $ch = curl_init();
+  
+  //set the url, number of POST vars, POST data
+  curl_setopt($ch,CURLOPT_URL, $url);
+  curl_setopt($ch,CURLOPT_POST, true);
+  curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "Authorization: Bearer PAYSTACK_SECRET_URL",
+    "Cache-Control: no-cache",
+  ));
+  
+  //So that curl_exec returns the contents of the cURL; rather than echoing it
+  curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+  
+  //execute post
+  $result = curl_exec($ch);
+  echo $result;
+?>
+
+//save user account details to database
+user->account_id = ""
+
+
+// split payment for resturant, bike service
+
+
+//foreach item in payment
+//get account details 
+
+<?php
+  $url = "https://api.paystack.co/transaction/initialize";
+
+  $fields = [
+    'email' => "customer@email.com",
+    'amount' => "20000",
+    'subaccount' => "ACCT_xxxxxxxxx"
+  ];
+
+  $fields_string = http_build_query($fields);
+
+  //open connection
+  $ch = curl_init();
+  
+  //set the url, number of POST vars, POST data
+  curl_setopt($ch,CURLOPT_URL, $url);
+  curl_setopt($ch,CURLOPT_POST, true);
+  curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "Authorization: Bearer PAYSTACK_SECRET_URL",
+    "Cache-Control: no-cache",
+  ));
+  
+  //So that curl_exec returns the contents of the cURL; rather than echoing it
+  curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+  
+  //execute post
+  $result = curl_exec($ch);
+  echo $result;
+?>
+
+<?php
+  $url = "https://api.paystack.co/transaction/initialize";
+
+  $fields = [
+    'email' => "customer@email.com",
+    'amount' => "20000",
+    'subaccount' => "ACCT_xxxxxxxxx",
+    'transaction_charge' => "10000"
+  ];
+
+  $fields_string = http_build_query($fields);
+
+  //open connection
+  $ch = curl_init();
+  
+  //set the url, number of POST vars, POST data
+  curl_setopt($ch,CURLOPT_URL, $url);
+  curl_setopt($ch,CURLOPT_POST, true);
+  curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "Authorization: Bearer SECRET_KEY",
+    "Cache-Control: no-cache",
+  ));
+  
+  //So that curl_exec returns the contents of the cURL; rather than echoing it
+  curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+  
+  //execute post
+  $result = curl_exec($ch);
+  echo $result;
+?>
